6c6d72f6c7c78784de030f7bb7056cb46572678e
Kushim-Jiang <894857367@qq.com>
2025-07-01 16:52:03 +0800

    Update xianhan.py

--- data/xianhan.py
+++ data/xianhan.py
@@ -28,35 +28,20 @@ class Dictionary:
             json.dump(result, f, ensure_ascii=False, indent=2)
 
 
-def save_xianhan():
-    XIANHAN_DIR = Path("D:/Documents/xianhan-7")
-    MDD_DIR = XIANHAN_DIR / "现汉7.mdd"
-    MDX_DIR = XIANHAN_DIR / "现汉7.mdx"
-    OUTPUT_DIR = XIANHAN_DIR / "output"
+def build(dict_dir: Path, name_stem: str):
+    MDD_DIR = dict_dir / f"{name_stem}.mdd"
+    MDX_DIR = dict_dir / f"{name_stem}.mdx"
+    OUTPUT_DIR = dict_dir / "output"
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
-    xianhan = Dictionary(MDD_DIR, MDX_DIR)
-    xianhan.save(OUTPUT_DIR)
+    dictionary = Dictionary(MDD_DIR, MDX_DIR)
+    dictionary.save(OUTPUT_DIR)
 
     output_str = (OUTPUT_DIR / "result.json").read_text(encoding="utf-8")
     with (OUTPUT_DIR / "result.txt").open("w", encoding="utf-8") as f:
         f.write("\n".join(set(output_str)))
 
 
-def save_xinhua():
-    XINHUA_DIR = Path("D:/Documents/xinhua-12")
-    MDD_DIR = XINHUA_DIR / "新华字典12版图声.mdd"
-    MDX_DIR = XINHUA_DIR / "新华字典12版图声.mdx"
-    OUTPUT_DIR = XINHUA_DIR / "output"
-    OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
-
-    xinhua = Dictionary(MDD_DIR, MDX_DIR)
-    xinhua.save(OUTPUT_DIR)
-
-    output_str = (OUTPUT_DIR / "result.json").read_text(encoding="utf-8")
-    with (OUTPUT_DIR / "result.txt").open("w", encoding="utf-8") as f:
-        f.write("\n".join(set(output_str)))
-
-
-if __name__ == "__main__":
-    save_xinhua()
+build(Path("D:/Documents/xianhan-7"), "现汉7")
+build(Path("D:/Documents/xinhua-12"), "新华字典12版图声")
+build(Path("D:/Documents/guhan-2"), "古代汉语词典2 (2014)")
